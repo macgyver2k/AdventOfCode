64c2e49a492b9ee33d5fb5546c9680be33ad0693
macgyver2k <christopher.kloss@ubimidum.de>
2020-12-02 23:58:12 +0100

    implemented day 02 part 2 (#5)

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -11,30 +11,51 @@ namespace Day2
         {
             var input = await File.ReadAllLinesAsync("input.txt");
 
-            var validPasswords = input
-                .Where(isPasswordValid)
+            var passwords = input
+                .Select( SplitPassword )
+                .ToList();
+
+            var validPasswordsPart1 = passwords
+                .Where(line => isPasswordValidPart1(line.min, line.max, line.key, line.password))
                 .Count();
 
-            Console.WriteLine( "Valid: {0}", validPasswords );
+            Console.WriteLine( "Valid #1: {0}", validPasswordsPart1 );
+
+            var validPasswordsPart2 = passwords
+                .Where(line => isPasswordValidPart2(line.min, line.max, line.key, line.password))
+                .Count();
+
+            Console.WriteLine("Valid #2: {0}", validPasswordsPart2);
         }
 
-        private static Boolean isPasswordValid(string line)
+        private static Boolean isPasswordValidPart1( Int32 min, Int32 max, Char key, String password)
         {
-            var parts = line.Split( 
-                new[] { '-', ' ', ':' },
-                StringSplitOptions.RemoveEmptyEntries 
-            );
-
-            var min = Convert.ToInt32(parts[0]);
-            var max = Convert.ToInt32(parts[1]);
-            var key = parts[2].First();
-            var password = parts[3];
-
             var amountOfKey = password
                 .Where(c => c == key)
                 .Count();
 
             return amountOfKey >= min && amountOfKey <= max;
         }
+
+        private static Boolean isPasswordValidPart2(Int32 min, Int32 max, Char key, String password)
+        {
+            return (password[min - 1] == key) ^ (password[max - 1] == key);
+        }
+
+        private static (Int32 min, Int32 max, Char key, String password) SplitPassword(string line)
+        {
+            var parts = line.Split(
+                new[] { '-', ' ', ':' },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+
+            return (
+                Convert.ToInt32(parts[0]),
+                Convert.ToInt32(parts[1]),
+                parts[2].First(),
+                parts[3]
+            );
+        }
+
     }
 }
