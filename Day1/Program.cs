0c40419cb09abe5068afd3fbca87fb64e3ad293d
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-12-02 22:38:17 +0100

    implemented day 01

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = await File.ReadAllLinesAsync( "input.txt" );
+
+            var numbers = input
+                .Select((line, index) => (index,value: Convert.ToInt32(line)))
+                .ToList();
+
+            var sums = numbers
+                .SelectMany(entry => numbers
+                   .Where(second => second.index != entry.index)
+                   .Select(second => (sum: second.value + entry.value, left: entry.value, right: second.value))
+                ).ToList();
+
+            var pair = sums.First(entry => entry.sum == 2020);
+            var result = pair.left * pair.right;
+
+            Console.WriteLine( "Result: {0}", result );
+        }
+    }
+}
