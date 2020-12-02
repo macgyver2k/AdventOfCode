f662e2efa089401b85e8c7ced5006b5944fcdf94
Christopher Kloß <christopher.kloss@netscrapers.com>
2020-12-02 23:14:48 +0100

    implemented day 02

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Day2
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = await File.ReadAllLinesAsync("input.txt");
+
+            var validPasswords = input
+                .Where(isPasswordValid)
+                .Count();
+
+            Console.WriteLine( "Valid: {0}", validPasswords );
+        }
+
+        private static Boolean isPasswordValid(string line)
+        {
+            var parts = line.Split( 
+                new[] { '-', ' ', ':' },
+                StringSplitOptions.RemoveEmptyEntries 
+            );
+
+            var min = Convert.ToInt32(parts[0]);
+            var max = Convert.ToInt32(parts[1]);
+            var key = parts[2].First();
+            var password = parts[3];
+
+            var amountOfKey = password
+                .Where(c => c == key)
+                .Count();
+
+            return amountOfKey >= min && amountOfKey <= max;
         }
     }
 }
