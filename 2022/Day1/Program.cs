e7b3ed80d0df497a2c4f2414833ab60e35ea1a6c
Christopher Kloß <christopher.kloss@netscrapers.com>
2022-12-08 00:37:10 +0100

    implemented day 01

---
+++ 2022/Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+var sum = 0;
+var max = 0;
+
+foreach (var line in inputLines)
+{
+    if ( !String.IsNullOrEmpty( line ) )
+    {
+        var number = Convert.ToInt32(line);
+        sum += number;
+        continue;
+    }        
+
+    if (sum > max)
+    {
+        max = sum;
+    }
+
+    sum = 0;
+}
+
+Console.WriteLine( $"Elve with most calories caries {max}" );
