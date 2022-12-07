ad5ce232584b5c05ca923dac28509393b3077b83
Christopher Kloß <christopher.kloss@netscrapers.com>
2022-12-08 00:50:29 +0100

    implemented day 01 part 2

--- 2022/Day1/Program.cs
+++ 2022/Day1/Program.cs
@@ -1,23 +1,58 @@
 ﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
 
-var sum = 0;
-var max = 0;
+var mostCalories = FindMostCalories(inputLines);
+Console.WriteLine($"Elve with most calories caries {mostCalories}");
 
-foreach (var line in inputLines)
+static int FindMostCalories(string[] inputLines)
 {
-    if ( !String.IsNullOrEmpty( line ) )
-    {
-        var number = Convert.ToInt32(line);
-        sum += number;
-        continue;
-    }        
+    var sum = 0;
+    var max = 0;
 
-    if (sum > max)
+    foreach (var line in inputLines)
     {
-        max = sum;
+        if (!String.IsNullOrEmpty(line))
+        {
+            var number = Convert.ToInt32(line);
+            sum += number;
+            continue;
+        }
+
+        if (sum > max)
+        {
+            max = sum;
+        }
+
+        sum = 0;
     }
 
-    sum = 0;
+    return max;
 }
 
-Console.WriteLine( $"Elve with most calories caries {max}" );
+var mostCaloriesTop3 = FindTop3MostCalories(inputLines);
+Console.WriteLine($"3 Elves with most calories carry {mostCaloriesTop3}");
+
+static int FindTop3MostCalories(string[] inputLines)
+{
+    var sum = 0;
+    var maxes = new[] { 0, 0, 0 };
+
+    foreach (var line in inputLines)
+    {
+        if (!String.IsNullOrEmpty(line))
+        {
+            var number = Convert.ToInt32(line);
+            sum += number;
+            continue;
+        }
+
+        maxes = maxes
+            .Concat(new[] { sum })
+            .OrderByDescending(_ => _)
+            .Take(3)
+            .ToArray();        
+        
+        sum = 0;
+    }
+
+    return maxes.Sum();
+}
