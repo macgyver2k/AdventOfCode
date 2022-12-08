1259f2f34c7b38e1173e5d1e65e0260f65ae8761
Christopher Kloß <christopher.kloss@netscrapers.com>
2022-12-08 13:39:32 +0100

    implemented day 04

---
+++ 2022/Day4/Program.cs
@@ -0,0 +1,31 @@
+﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+var sum = 0;
+
+foreach (var line in inputLines)
+{
+    var ranges = line
+        .Split(',')
+        .Select(x =>
+        {
+            var y = x
+                .Split('-')
+                .Select(_ => Convert.ToInt32(_))
+                .ToArray();
+
+            return (Left: y[0], Right: y[1]);
+        })
+        .OrderBy(_ => _.Left)
+        .ThenByDescending(_ => _.Right)
+        .ToArray();
+
+    var isLeft = ranges[1].Left >= ranges[0].Left;
+    var isRight = ranges[1].Right <= ranges[0].Right;
+
+    if (isLeft && isRight)
+    {
+        sum++;
+    }        
+}
+
+Console.WriteLine($"Amount of overlapping pairs is {sum}");
