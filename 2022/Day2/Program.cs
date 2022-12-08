af33e09102601bc2f01c4541a9fb546528e14033
Christopher Kloß <christopher.kloss@netscrapers.com>
2022-12-08 01:19:08 +0100

    implemented day 02

---
+++ 2022/Day2/Program.cs
@@ -0,0 +1,54 @@
+﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+var sum = CalculateTotalScore(inputLines);
+Console.WriteLine($"Total score is {sum}");
+
+Int32 GetShapeScore(Char me) => me switch
+{
+    'X' => 1,
+    'Y' => 2,
+    'Z' => 3,
+};
+
+Int32 GetOutcomeScore(Char opponent, Char me)
+{
+    return me switch
+    {
+        'X' => opponent switch
+        {
+            'A' => 3,
+            'B' => 0,
+            'C' => 6,
+        },
+        'Y' => opponent switch
+        {
+            'A' => 6,
+            'B' => 3,
+            'C' => 0,
+        },
+        'Z' => opponent switch
+        {
+            'A' => 0,
+            'B' => 6,
+            'C' => 3,
+        },
+    };
+}
+
+int CalculateTotalScore(string[] inputLines)
+{
+    var sum = 0;
+
+    foreach (var line in inputLines)
+    {
+        var opponent = line[0];
+        var me = line[2];
+
+        var myScore = GetShapeScore(me);
+        var outcomeScore = GetOutcomeScore(opponent, me);
+
+        sum += (myScore + outcomeScore);
+    }
+
+    return sum;
+}
