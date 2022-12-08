728af7547fb6f44a2909a35b01dadeeabc726994
Christopher Kloß <christopher.kloss@netscrapers.com>
2022-12-08 02:31:27 +0100

    Feature/2022 day 01 to 03 (#13)
    * implemented day 01
    
    * implemented day 01 part 2
    
    * implemented day 02
    
    * implemented day 02 part 2
    
    * implemented day 03

---
+++ 2022/Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+var mostCalories = FindMostCalories(inputLines);
+Console.WriteLine($"Elve with most calories caries {mostCalories}");
+
+static int FindMostCalories(string[] inputLines)
+{
+    var sum = 0;
+    var max = 0;
+
+    foreach (var line in inputLines)
+    {
+        if (!String.IsNullOrEmpty(line))
+        {
+            var number = Convert.ToInt32(line);
+            sum += number;
+            continue;
+        }
+
+        if (sum > max)
+        {
+            max = sum;
+        }
+
+        sum = 0;
+    }
+
+    return max;
+}
+
+var mostCaloriesTop3 = FindTop3MostCalories(inputLines);
+Console.WriteLine($"3 Elves with most calories carry {mostCaloriesTop3}");
+
+static int FindTop3MostCalories(string[] inputLines)
+{
+    var sum = 0;
+    var maxes = new[] { 0, 0, 0 };
+
+    foreach (var line in inputLines)
+    {
+        if (!String.IsNullOrEmpty(line))
+        {
+            var number = Convert.ToInt32(line);
+            sum += number;
+            continue;
+        }
+
+        maxes = maxes
+            .Concat(new[] { sum })
+            .OrderByDescending(_ => _)
+            .Take(3)
+            .ToArray();        
+        
+        sum = 0;
+    }
+
+    return maxes.Sum();
+}
---
+++ 2022/Day3/Program.cs
@@ -0,0 +1,55 @@
+﻿var inputLines = await File.ReadAllLinesAsync("input.txt");
+
+var sum = CalculatePrioritySum(inputLines);
+Console.WriteLine($"The sum of priorities is {sum}");
+
+static int CalculatePrioritySum(string[] inputLines)
+{
+    var sum = 0;
+
+    foreach (var line in inputLines)
+    {
+        var chars = line.ToArray();
+        var mid = chars.Length / 2;
+        var left = chars[..mid];
+        var right = chars[mid..];
+
+        var common = left.First(right.Contains);
+        var priority = common - (Char.IsLower(common) ? 96 : 64 - 26);
+
+        sum += priority;
+    }
+
+    return sum;
+}
+
+var groupSum = CalculatePrioritySumGrouped(inputLines);
+Console.WriteLine($"The sum of grouped priorities is {groupSum}");
+
+
+static int CalculatePrioritySumGrouped(string[] inputLines)
+{
+    var sum = 0;
+    var buffer = new List<String>();
+
+    foreach (var line in inputLines)
+    {
+        if( buffer.Count < 3 )
+        {
+            buffer.Add(line);
+        }
+
+        if (buffer.Count < 3)
+        {
+            continue;
+        }
+
+        var common = buffer.Select(_ => _.ToArray()).Aggregate((a, b) => a.Where(b.Contains).ToArray()).First();        
+        var priority = common - (Char.IsLower(common) ? 96 : 64 - 26);
+
+        sum += priority;
+        buffer.Clear();
+    }
+
+    return sum;
+}
